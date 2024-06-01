93c7a51b97def936cd928dddfd7d5f9cfcaa0648
Luxmi <luxmi5829@gmail.com>
2024-06-01 23:45:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return  <h1 className="text-3xl font-bold underline">
+  return  <h1 className="text-3xl font-bold text-blue-500 ">
   Hello world!
 </h1>
 }
