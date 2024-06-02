376809ef066a4e3ecbadb66826e25e7b6168c024
Luxmi <luxmi5829@gmail.com>
2024-06-03 02:00:18 +0530

    add new files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const HomePage = () => {
+  return <h1>Home Page</h1>;
+};
 function App() {
-  return  <h1 className="text-3xl font-bold text-blue-500 ">
-  Hello world!
-</h1>
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<HomePage />}/>
+
+   </Routes>
+   </BrowserRouter>
+  );
 }
 
 export default App;
