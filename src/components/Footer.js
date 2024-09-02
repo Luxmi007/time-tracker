19ef03f8b21c1fb05feefe03be4f95c03b5a2336
Luxmi <luxmi5829@gmail.com>
2024-09-03 00:28:13 +0530

    report

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -80,7 +80,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2024 Inovotek Coding Academy, Inc. All rights reserved.
+            &copy; Time tracker 2024, Inc. All rights reserved.
           </p>
         </div>
       </div>
