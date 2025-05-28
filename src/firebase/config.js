c5f85f1ed9a05896c23ea389e01263c2beef3f44
Luxmi <94300957+Luxmi007@users.noreply.github.com>
2025-05-28 16:22:20 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -10,12 +10,6 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  authDomain: "time-tracker-a2c64.firebaseapp.com",
-  projectId: "time-tracker-a2c64",
-  storageBucket: "time-tracker-a2c64.appspot.com",
-  messagingSenderId: "479916850967",
-  appId: "1:479916850967:web:e850432e005e1f02b14152"
 };
 
 // Initialize Firebase
