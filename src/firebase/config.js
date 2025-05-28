d4ce4c8ab1e9232081d526bfaa6fd49a8d52ff16
luxmi007 <luxmi5829@gmail.com>
2025-05-28 15:47:29 +0530

    updated config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,15 +5,15 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDjRMgfbyGsu3q-cz13S_2awovUbVLF2eE",
-  authDomain: "time-tracker-a2c64.firebaseapp.com",
-  projectId: "time-tracker-a2c64",
-  storageBucket: "time-tracker-a2c64.appspot.com",
-  messagingSenderId: "479916850967",
-  appId: "1:479916850967:web:e850432e005e1f02b14152"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app; 
+export default app;
